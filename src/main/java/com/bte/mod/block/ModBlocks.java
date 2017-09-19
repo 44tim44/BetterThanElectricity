323b42ce70011d36caa198a1e13c73e681043f19
Timeout <timeisrelative94@gmail.com>
2017-09-19 16:27:47 +0200

    Added color planks

--- src/main/java/com/bte/mod/block/ModBlocks.java
+++ src/main/java/com/bte/mod/block/ModBlocks.java
@@ -31,6 +31,24 @@ public class ModBlocks {
     public static BlockBase machineblock = new BlockBase(Material.IRON,"machine_block").setCreativeTab(CreativeTabs.DECORATIONS);
     public static BlockSolarPanel solarPanel = new BlockSolarPanel();
 
+    public static BlockBase planks_white = new BlockBase(Material.WOOD,"planks_white").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    public static BlockBase planks_lightgray = new BlockBase(Material.WOOD,"planks_lightgray").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    public static BlockBase planks_gray = new BlockBase(Material.WOOD,"planks_gray").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    public static BlockBase planks_black = new BlockBase(Material.WOOD,"planks_black").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    public static BlockBase planks_brown = new BlockBase(Material.WOOD,"planks_brown").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    public static BlockBase planks_red = new BlockBase(Material.WOOD,"planks_red").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    public static BlockBase planks_orange = new BlockBase(Material.WOOD,"planks_orange").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    public static BlockBase planks_yellow = new BlockBase(Material.WOOD,"planks_yellow").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    public static BlockBase planks_lime = new BlockBase(Material.WOOD,"planks_lime").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    public static BlockBase planks_green = new BlockBase(Material.WOOD,"planks_green").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    public static BlockBase planks_cyan = new BlockBase(Material.WOOD,"planks_cyan").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    public static BlockBase planks_lightblue = new BlockBase(Material.WOOD,"planks_lightblue").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    public static BlockBase planks_blue = new BlockBase(Material.WOOD,"planks_blue").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    public static BlockBase planks_purple = new BlockBase(Material.WOOD,"planks_purple").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    public static BlockBase planks_magenta = new BlockBase(Material.WOOD,"planks_magenta").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+    public static BlockBase planks_pink = new BlockBase(Material.WOOD,"planks_pink").setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+
+
     public static void register(IForgeRegistry<Block> registry) {
         registry.registerAll(
                 oreCopper,
@@ -43,7 +61,24 @@ public class ModBlocks {
                 electricFurnace,
                 electricFurnace_on,
                 machineblock,
-                solarPanel
+                solarPanel,
+
+                planks_white,
+                planks_lightgray,
+                planks_gray,
+                planks_black,
+                planks_brown,
+                planks_red,
+                planks_orange,
+                planks_yellow,
+                planks_lime,
+                planks_green,
+                planks_cyan,
+                planks_lightblue,
+                planks_blue,
+                planks_purple,
+                planks_magenta,
+                planks_pink
         );
 
         GameRegistry.registerTileEntity(pulverizer.getTileEntityClass(), pulverizer.getRegistryName().toString());
@@ -62,7 +97,23 @@ public class ModBlocks {
                 electricFurnace.createItemBlock(),
                 electricFurnace_on.createItemBlock(),
                 machineblock.createItemBlock(),
-                solarPanel.createItemBlock()
+                solarPanel.createItemBlock(),
+                planks_white.createItemBlock(),
+                planks_lightgray.createItemBlock(),
+                planks_gray.createItemBlock(),
+                planks_black.createItemBlock(),
+                planks_brown.createItemBlock(),
+                planks_red.createItemBlock(),
+                planks_orange.createItemBlock(),
+                planks_yellow.createItemBlock(),
+                planks_lime.createItemBlock(),
+                planks_green.createItemBlock(),
+                planks_cyan.createItemBlock(),
+                planks_lightblue.createItemBlock(),
+                planks_blue.createItemBlock(),
+                planks_purple.createItemBlock(),
+                planks_magenta.createItemBlock(),
+                planks_pink.createItemBlock()
         );
     }
 
@@ -77,6 +128,23 @@ public class ModBlocks {
         electricFurnace_on.registerItemModel(Item.getItemFromBlock(electricFurnace_on));
         machineblock.registerItemModel(Item.getItemFromBlock(machineblock));
         solarPanel.registerItemModel(Item.getItemFromBlock(solarPanel));
+
+        planks_white.registerItemModel(Item.getItemFromBlock(planks_white));
+        planks_lightgray.registerItemModel(Item.getItemFromBlock(planks_lightgray));
+        planks_gray.registerItemModel(Item.getItemFromBlock(planks_gray));
+        planks_black.registerItemModel(Item.getItemFromBlock(planks_black));
+        planks_brown.registerItemModel(Item.getItemFromBlock(planks_brown));
+        planks_red.registerItemModel(Item.getItemFromBlock(planks_red));
+        planks_orange.registerItemModel(Item.getItemFromBlock(planks_orange));
+        planks_yellow.registerItemModel(Item.getItemFromBlock(planks_yellow));
+        planks_lime.registerItemModel(Item.getItemFromBlock(planks_lime));
+        planks_green.registerItemModel(Item.getItemFromBlock(planks_green));
+        planks_cyan.registerItemModel(Item.getItemFromBlock(planks_cyan));
+        planks_lightblue.registerItemModel(Item.getItemFromBlock(planks_lightblue));
+        planks_blue.registerItemModel(Item.getItemFromBlock(planks_blue));
+        planks_purple.registerItemModel(Item.getItemFromBlock(planks_purple));
+        planks_magenta.registerItemModel(Item.getItemFromBlock(planks_magenta));
+        planks_pink.registerItemModel(Item.getItemFromBlock(planks_pink));
     }
 
 }
